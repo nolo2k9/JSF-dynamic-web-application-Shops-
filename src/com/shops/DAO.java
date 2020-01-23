136e100bb65fcd979c666e49bc7e60a0a53259a3
Keith Nolan <g00351932@gmit.ie>
2020-01-23 13:53:20 +0000

    Initial commit

---
+++ src/com/shops/DAO.java
@@ -0,0 +1,22 @@
+package com.shops;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+
+public class DAO {
+
+	private DataSource mysqlDS;
+
+	
+	/* ======================================================================================================
+	 * Constructor
+	 * ====================================================================================================== */
+	public DAO() throws Exception {
+		Context context = new InitialContext();
+		String jndiName = "java:comp/env/shops";
+		mysqlDS = (DataSource) context.lookup(jndiName);
+	}
+
+}
